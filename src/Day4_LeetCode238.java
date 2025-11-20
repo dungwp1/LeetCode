d44a530abf0094c18dddc6ded291087056467628
dungwp <dungwp1@gmail.com>
2025-11-20 15:16:38 +0700

    Day4_LeetCode238

---
+++ src/Day4_LeetCode238.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+public class Day4_LeetCode238 {
+    public static int[] productExceptSelf(int[] nums) {
+        int[] answer = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            answer[i] = 1;
+        }
+        int[] right = Arrays.copyOf(answer, answer.length);
+        for (int i = 1; i < nums.length; i++) {
+
+            answer[i] = answer[i - 1] * nums[i - 1];
+        }
+        System.out.println("Check left" + Arrays.toString(answer));
+        for (int i = nums.length - 2; i >= 0; i--) {
+            right[i] = nums[i + 1] * right[i + 1];
+            answer[i] *= right[i];
+        }
+        System.out.println("Check right" + Arrays.toString(right));
+
+        return answer;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
 //        int[] prices1 = {7, 1, 5, 3, 6, 4};
@@ -10,15 +12,12 @@ public class Main {
 //        int result3 = Day1_LeetCode121.maxProfit(prices3);
 //        System.out.println(result3);
 
-        int[] num1 = {1, 2, 3, 1};
         int[] num2 = {1, 2, 3, 4};
-        int[] num3 = {1, 1, 1, 3, 3, 4, 3, 2, 4, 2};
+        int[] num3 = {-1, 1, 0, -3, 3};
 
-        boolean result1 = Day2_LeetCode217.containsDuplicate(num1);
-        boolean result2 = Day2_LeetCode217.containsDuplicate(num2);
-        boolean result3 = Day2_LeetCode217.containsDuplicate(num3);
-        System.out.println(result1);
-        System.out.println(result2);
-        System.out.println(result3);
+        int[] result2 = Day4_LeetCode238.productExceptSelf(num2);
+        int[] result3 = Day4_LeetCode238.productExceptSelf(num3);
+        System.out.println(Arrays.toString(result2));
+        System.out.println(Arrays.toString(result3));
     }
 }
