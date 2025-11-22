450050fd646ee933bc6a84fdffa1e4532b2677de
dungwp <dungwp1@gmail.com>
2025-11-23 00:57:25 +0700

    Day6_LeetCode153

---
+++ src/Day6_LeetCode153.java
@@ -0,0 +1,17 @@
+public class Day6_LeetCode153 {
+    public static int findMin(int[] nums) {
+        int left = 0, right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] > nums[right]) {
+                left = mid + 1;     // min nằm bên phải
+            } else {
+                right = mid;        // min nằm bên trái hoặc chính mid
+            }
+        }
+
+        return nums[left];
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -12,12 +12,18 @@ public class Main {
 //        int result3 = Day1_LeetCode121.maxProfit(prices3);
 //        System.out.println(result3);
 
-        int[] num2 = {1, 1, 1};
-        int[] num3 = {1, 2, 3};
+        int[] num1 = {1, 2};
+        int[] num2 = {4, 5, 6, 7, 0, 1, 2};
+        int[] num3 = {11, 13, 15, 17};
+        int[] num4 = {3, 4, 5, 1, 2};
 
-        int result2 = Day5_LeetCode560.subarraySum(num2, 2);
-        int result3 = Day5_LeetCode560.subarraySum(num3, 3);
+        int result1 = Day6_LeetCode153.findMin(num1);
+        System.out.println(result1);
+        int result2 = Day6_LeetCode153.findMin(num2);
         System.out.println(result2);
+        int result3 = Day6_LeetCode153.findMin(num3);
         System.out.println(result3);
+        int result4 = Day6_LeetCode153.findMin(num4);
+        System.out.println(result4);
     }
 }
