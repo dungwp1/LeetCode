9c18c960d566d395ea45ae6a26b4223d21a77ec2
dungwp <dungwp1@gmail.com>
2025-11-24 23:57:29 +0700

    Day7_LeetCode33

--- src/Main.java
+++ src/Main.java
@@ -12,18 +12,14 @@ public class Main {
 //        int result3 = Day1_LeetCode121.maxProfit(prices3);
 //        System.out.println(result3);
 
-        int[] num1 = {1, 2};
-        int[] num2 = {4, 5, 6, 7, 0, 1, 2};
-        int[] num3 = {11, 13, 15, 17};
-        int[] num4 = {3, 4, 5, 1, 2};
+        int[] num1 = {4, 5, 6, 7, 0, 1, 2};
+        int[] num2 = {1};
 
-        int result1 = Day6_LeetCode153.findMin(num1);
+        int result1 = Day7_LeetCode33.search(num1, 0);
         System.out.println(result1);
-        int result2 = Day6_LeetCode153.findMin(num2);
+        int result2 = Day7_LeetCode33.search(num1, 3);
         System.out.println(result2);
-        int result3 = Day6_LeetCode153.findMin(num3);
+        int result3 = Day7_LeetCode33.search(num2, 0);
         System.out.println(result3);
-        int result4 = Day6_LeetCode153.findMin(num4);
-        System.out.println(result4);
     }
 }
