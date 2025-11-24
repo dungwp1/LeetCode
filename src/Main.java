6cb3b5ae14aadb48bb46b4ce05bfa6059ac06cd2
dungwp <dungwp1@gmail.com>
2025-11-25 00:43:25 +0700

    Day8_LeetCode81

--- src/Main.java
+++ src/Main.java
@@ -12,14 +12,12 @@ public class Main {
 //        int result3 = Day1_LeetCode121.maxProfit(prices3);
 //        System.out.println(result3);
 
-        int[] num1 = {4, 5, 6, 7, 0, 1, 2};
-        int[] num2 = {1};
+        int[] num1 = {2, 5, 6, 0, 0, 1, 2};
+        int[] num2 = {2, 5, 6, 0, 0, 1, 2};
 
-        int result1 = Day7_LeetCode33.search(num1, 0);
+        boolean result1 = Day8_LeetCode81.search(num1, 0);
         System.out.println(result1);
-        int result2 = Day7_LeetCode33.search(num1, 3);
+        boolean result2 = Day8_LeetCode81.search(num1, 3);
         System.out.println(result2);
-        int result3 = Day7_LeetCode33.search(num2, 0);
-        System.out.println(result3);
     }
 }
