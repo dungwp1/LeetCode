046cb34dbfb78975dd71454f43f0e87fa40932a8
DungWP <dungwp@172-15-195-117.lightspeed.bcvloh.sbcglobal.net>
2025-12-30 15:59:36 +0700

    D16_125

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        int[] nums1 = {3, 2, 1};
-        int[] nums2 = {1, 2};
-        int[] nums3 = {2, 2, 3, 1};
-        int[] nums4 = {1, 2, -2147483648};
-
-
-        System.out.println("--------------" + D15_414.thirdMax(nums1));
-        System.out.println("--------------" + D15_414.thirdMax(nums2));
-        System.out.println("--------------" + D15_414.thirdMax(nums3));
-        System.out.println("--------------" + D15_414.thirdMax(nums4));
+        System.out.println("---" + D16_125.isPalindrome("A man, a plan, a canal: Panama"));
+        System.out.println("---" + D16_125.isPalindrome("race a car"));
+        System.out.println("---" + D16_125.isPalindrome(" "));
     }
 }
